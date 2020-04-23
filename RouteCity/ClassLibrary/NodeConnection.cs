0ecc3cbc24728bb621accb03a057f801a1b92220
Lydia Lind <lydia.nilsson@gmail.com>
2020-04-23 10:30:59 +0200

    Initial commit - Setting up program skeleton

---
+++ RouteCity/ClassLibrary/NodeConnection.cs
@@ -0,0 +1,7 @@
+﻿namespace ClassLibrary
+{
+    class NoceConnection
+    {
+
+    }
+}
---
+++ RouteCity/ClassLibrary/Path.cs
@@ -0,0 +1,19 @@
+﻿namespace ClassLibrary
+{
+    class Path
+    {
+        //PROPERTIES
+        internal Node Node { get; set; }
+        internal double ShortestTimeFromStartNode { get; set; }
+        internal Node PreviousNode { get; set; }
+
+        //CONSTRUCTOR
+        public Path(Node node, double shortestTimeFromStartNode = double.PositiveInfinity, Node previousNode = null)
+        {
+            Node = node;
+            ShortestTimeFromStartNode = shortestTimeFromStartNode;
+            PreviousNode = previousNode;
+        }
+
+    }
+}
