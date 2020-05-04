2fb76d51f080166a518f744e63f5f3d1b958b424
Lime Fortytwo <melberik@gmail.com>
2020-05-04 14:38:02 +0200

    Uppdaterade med Formsversion

---
+++ RouteCity/FormsVersion/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace FormsVersion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.nodeA = new System.Windows.Forms.PictureBox();
+            this.nodeC = new System.Windows.Forms.PictureBox();
+            this.nodeD = new System.Windows.Forms.PictureBox();
+            this.nodeE = new System.Windows.Forms.PictureBox();
+            this.nodeF = new System.Windows.Forms.PictureBox();
+            this.nodeG = new System.Windows.Forms.PictureBox();
+            this.nodeH = new System.Windows.Forms.PictureBox();
+            this.nodeI = new System.Windows.Forms.PictureBox();
+            this.nodeJ = new System.Windows.Forms.PictureBox();
+            this.nodeB = new System.Windows.Forms.PictureBox();
+            this.gbxControl = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeC)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeE)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeF)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeH)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeI)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeJ)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nodeA
+            // 
+            this.nodeA.BackColor = System.Drawing.Color.Transparent;
+            this.nodeA.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nodeA.BackgroundImage")));
+            this.nodeA.Location = new System.Drawing.Point(377, 43);
+            this.nodeA.Name = "nodeA";
+            this.nodeA.Size = new System.Drawing.Size(100, 98);
+            this.nodeA.TabIndex = 0;
+            this.nodeA.TabStop = false;
+            // 
+            // nodeC
+            // 
+            this.nodeC.BackColor = System.Drawing.Color.Transparent;
+            this.nodeC.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nodeC.BackgroundImage")));
+            this.nodeC.Location = new System.Drawing.Point(596, 106);
+            this.nodeC.Name = "nodeC";
+            this.nodeC.Size = new System.Drawing.Size(100, 98);
+            this.nodeC.TabIndex = 1;
+            this.nodeC.TabStop = false;
+            this.nodeC.MouseMove += new System.Windows.Forms.MouseEventHandler(this.nodeC_MouseMove);
+            // 
+            // nodeD
+            // 
+            this.nodeD.BackColor = System.Drawing.Color.Transparent;
+            this.nodeD.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nodeD.BackgroundImage")));
+            this.nodeD.Location = new System.Drawing.Point(638, 218);
+            this.nodeD.Name = "nodeD";
+            this.nodeD.Size = new System.Drawing.Size(100, 98);
+            this.nodeD.TabIndex = 2;
+            this.nodeD.TabStop = false;
+            // 
+            // nodeE
+            // 
+            this.nodeE.BackColor = System.Drawing.Color.Transparent;
+            this.nodeE.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nodeE.BackgroundImage")));
+            this.nodeE.Location = new System.Drawing.Point(596, 329);
+            this.nodeE.Name = "nodeE";
+            this.nodeE.Size = new System.Drawing.Size(100, 98);
+            this.nodeE.TabIndex = 3;
+            this.nodeE.TabStop = false;
+            // 
+            // nodeF
+            // 
+            this.nodeF.BackColor = System.Drawing.Color.Transparent;
+            this.nodeF.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nodeF.BackgroundImage")));
+            this.nodeF.Location = new System.Drawing.Point(491, 406);
+            this.nodeF.Name = "nodeF";
+            this.nodeF.Size = new System.Drawing.Size(100, 98);
+            this.nodeF.TabIndex = 4;
+            this.nodeF.TabStop = false;
+            // 
+            // nodeG
+            // 
+            this.nodeG.BackColor = System.Drawing.Color.Transparent;
+            this.nodeG.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nodeG.BackgroundImage")));
+            this.nodeG.Location = new System.Drawing.Point(377, 406);
+            this.nodeG.Name = "nodeG";
+            this.nodeG.Size = new System.Drawing.Size(100, 98);
+            this.nodeG.TabIndex = 5;
+            this.nodeG.TabStop = false;
+            // 
+            // nodeH
+            // 
+            this.nodeH.BackColor = System.Drawing.Color.Transparent;
+            this.nodeH.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nodeH.BackgroundImage")));
+            this.nodeH.Location = new System.Drawing.Point(272, 329);
+            this.nodeH.Name = "nodeH";
+            this.nodeH.Size = new System.Drawing.Size(100, 98);
+            this.nodeH.TabIndex = 6;
+            this.nodeH.TabStop = false;
+            // 
+            // nodeI
+            // 
+            this.nodeI.BackColor = System.Drawing.Color.Transparent;
+            this.nodeI.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nodeI.BackgroundImage")));
+            this.nodeI.Location = new System.Drawing.Point(225, 218);
+            this.nodeI.Name = "nodeI";
+            this.nodeI.Size = new System.Drawing.Size(100, 98);
+            this.nodeI.TabIndex = 7;
+            this.nodeI.TabStop = false;
+            // 
+            // nodeJ
+            // 
+            this.nodeJ.BackColor = System.Drawing.Color.Transparent;
+            this.nodeJ.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nodeJ.BackgroundImage")));
+            this.nodeJ.Location = new System.Drawing.Point(272, 106);
+            this.nodeJ.Name = "nodeJ";
+            this.nodeJ.Size = new System.Drawing.Size(100, 98);
+            this.nodeJ.TabIndex = 8;
+            this.nodeJ.TabStop = false;
+            // 
+            // nodeB
+            // 
+            this.nodeB.BackColor = System.Drawing.Color.Transparent;
+            this.nodeB.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("nodeB.BackgroundImage")));
+            this.nodeB.Location = new System.Drawing.Point(491, 43);
+            this.nodeB.Name = "nodeB";
+            this.nodeB.Size = new System.Drawing.Size(100, 98);
+            this.nodeB.TabIndex = 9;
+            this.nodeB.TabStop = false;
+            // 
+            // gbxControl
+            // 
+            this.gbxControl.BackColor = System.Drawing.Color.Black;
+            this.gbxControl.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.gbxControl.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbxControl.ForeColor = System.Drawing.Color.White;
+            this.gbxControl.Location = new System.Drawing.Point(0, 542);
+            this.gbxControl.Name = "gbxControl";
+            this.gbxControl.Size = new System.Drawing.Size(939, 120);
+            this.gbxControl.TabIndex = 10;
+            this.gbxControl.TabStop = false;
+            this.gbxControl.Text = "Controls";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FormsVersion.Properties.Resources.background2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(939, 662);
+            this.Controls.Add(this.gbxControl);
+            this.Controls.Add(this.nodeB);
+            this.Controls.Add(this.nodeJ);
+            this.Controls.Add(this.nodeI);
+            this.Controls.Add(this.nodeH);
+            this.Controls.Add(this.nodeG);
+            this.Controls.Add(this.nodeF);
+            this.Controls.Add(this.nodeE);
+            this.Controls.Add(this.nodeD);
+            this.Controls.Add(this.nodeC);
+            this.Controls.Add(this.nodeA);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nodeA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeC)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeE)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeF)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeH)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeI)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeJ)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nodeB)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox nodeA;
+        private System.Windows.Forms.PictureBox nodeC;
+        private System.Windows.Forms.PictureBox nodeD;
+        private System.Windows.Forms.PictureBox nodeE;
+        private System.Windows.Forms.PictureBox nodeF;
+        private System.Windows.Forms.PictureBox nodeG;
+        private System.Windows.Forms.PictureBox nodeH;
+        private System.Windows.Forms.PictureBox nodeI;
+        private System.Windows.Forms.PictureBox nodeJ;
+        private System.Windows.Forms.PictureBox nodeB;
+        private System.Windows.Forms.GroupBox gbxControl;
+    }
+}
+
---
+++ RouteCity/FormsVersion/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClassLibrary;
+
+namespace FormsVersion
+{
+    public partial class Form1 : Form
+    {
+        public Dictionary<PictureBox, Point> positions = new Dictionary<PictureBox, Point>();
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+
+            positions.Clear();
+            positions.Add(nodeA, CalculateAnchorPoint(new Point(479, 254), nodeA));
+            positions.Add(nodeB, CalculateAnchorPoint(new Point(479, 254), nodeB));
+            positions.Add(nodeC, CalculateAnchorPoint(new Point(479, 254), nodeC));
+            positions.Add(nodeD, CalculateAnchorPoint(new Point(479, 254), nodeD));
+            positions.Add(nodeE, CalculateAnchorPoint(new Point(479, 254), nodeE));
+            positions.Add(nodeF, CalculateAnchorPoint(new Point(479, 254), nodeF));
+            positions.Add(nodeG, CalculateAnchorPoint(new Point(479, 254), nodeG));
+            positions.Add(nodeH, CalculateAnchorPoint(new Point(479, 254), nodeH));
+            positions.Add(nodeI, CalculateAnchorPoint(new Point(479, 254), nodeI));
+            positions.Add(nodeJ, CalculateAnchorPoint(new Point(479, 254), nodeJ));
+
+            Connect(nodeA, nodeC, g);
+            Connect(nodeA, nodeD, g);
+            Connect(nodeA, nodeI, g);
+
+        }
+
+        public void Connect(PictureBox nodeOne, PictureBox nodeTwo, Graphics g)
+        {
+            Pen pen = new Pen(Color.White);
+            pen.Width = 2;
+            g.DrawLine(pen, positions[nodeOne].X, positions[nodeOne].Y, positions[nodeTwo].X, positions[nodeTwo].Y);
+        }
+
+        public Point CalculateAnchorPoint(Point middle, PictureBox node)
+        {
+            double xEnd = middle.X;
+            double yEnd = middle.Y;
+            double middleOfNodeX = node.Location.X + (node.Size.Width / 2);
+            double middleOfNodeY = node.Location.Y + (node.Size.Height / 2);
+
+            double angle = Math.Atan2((yEnd - middleOfNodeY), (xEnd - middleOfNodeX)) * (180 / Math.PI);
+            double radius = 40;
+
+            double x1 = middleOfNodeX + radius * Math.Cos(angle * (Math.PI / 180));
+            double y1 = middleOfNodeY + radius * Math.Sin(angle * (Math.PI / 180));
+            return new Point((int)x1, (int)y1);
+        }
+
+        private void nodeC_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                nodeC.Location = new Point(this.PointToClient(Cursor.Position).X - 50, this.PointToClient(Cursor.Position).Y - 50);
+                this.Refresh();
+            }
+        }
+    }
+}
